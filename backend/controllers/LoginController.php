8570a4ca13180842ee6a8a9253c347a4a457c63b
liliankuan <1162864381@qq.com>
2018-04-23 19:58:59 +0800

    first

---
+++ backend/controllers/LoginController.php
@@ -0,0 +1,13 @@
+<?php 
+namespace backend\controllers;
+use yii;
+class LoginController extends BaseController
+ {
+ 	public function actionLogin()
+ 	{
+ 		return $this->render('login');
+ 	}
+ 	
+ }
+
+ ?>
